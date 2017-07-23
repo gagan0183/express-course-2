fd74474b8059a3c694e46c412cb4c3f91c2cfd06
Gagan <g.deepsingh1@gmail.com>
2017-07-23 14:49:36 +0530

    complete gulp

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.port || 9000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/', function(req, res) {
+  res.send('Hello');
+});
+app.listen(port, function(err) {
+  console.log('running server on port ' + port);
+});
